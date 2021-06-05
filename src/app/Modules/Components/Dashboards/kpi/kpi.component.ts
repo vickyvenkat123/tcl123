9941713f116f837193f1b52a1def6a1a4bb6b5f9
RajeshBunny <aryanrajesh600@gmail.com>
2021-06-05 13:00:15 +0530

    new added changes

--- src/app/Modules/Components/Dashboards/kpi/kpi.component.ts
+++ src/app/Modules/Components/Dashboards/kpi/kpi.component.ts
@@ -1,15 +1,52 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ChartType } from 'chart.js';
 @Component({
   selector: 'app-kpi',
   templateUrl: './kpi.component.html',
   styleUrls: ['./kpi.component.css']
 })
 export class KpiComponent implements OnInit {
+  chartOptions: any;
+  chartColors: any;
+  doughnutChartLabels: string[] = ['Hot Zones', 'Live Zones'];
+  demodoughnutChartData: number[] = [7280, 1];
+  doughnutChartType: ChartType = 'doughnut';
+  pageNo: number = 0;
+  size: number = 10;
+  sizeChanged: boolean = false;
+  totalNoOfPages: number = 0;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.chartOptions = {
+      responsive: true,
+      cutoutPercentage: 90,
+      maintainAspectRatio: false,
+      title: {
+        display: true,
+        fontFamily: "quicksand-medium",
+        fontSize: 16,
+        fontColor: "#747d8c"
+      },
+      legend: {
+        display: true
+      },
+      tooltips: {
+        displayColors: false
+      }
+    };
+
+  }
+
+  // events
+  chartClicked(e: any): void {
+    //console.log(e);
+  }
+
+
+  chartHovered(e: any): void {
+    //console.log(e);
   }
 
 }
