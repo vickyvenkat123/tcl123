1c3012c37825d861035dd3307424532d67f74184
RajeshBunny <aryanrajesh600@gmail.com>
2021-06-06 01:40:49 +0530

    added new files

--- src/app/Modules/Components/Dashboards/kpi/kpi.component.ts
+++ src/app/Modules/Components/Dashboards/kpi/kpi.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ChartType } from 'chart.js';
+import { ChartType, ChartDataSets, ChartOptions } from 'chart.js';
+import { Color, Label, SingleDataSet } from 'ng2-charts';
+
 @Component({
   selector: 'app-kpi',
   templateUrl: './kpi.component.html',
@@ -15,7 +17,7 @@ export class KpiComponent implements OnInit {
   size: number = 10;
   sizeChanged: boolean = false;
   totalNoOfPages: number = 0;
-
+  lineChartType: ChartType = 'line';
   constructor() { }
 
   ngOnInit(): void {
@@ -47,6 +49,48 @@ export class KpiComponent implements OnInit {
 
   chartHovered(e: any): void {
     //console.log(e);
+  }
+
+
+  lineChartData: ChartDataSets[] = [
+    { data: [1, 3, 27, 8, 12, 5], label: 'Hazardous' },
+    { data: [0, 13, 10, 16, 6, 2], label: 'SOS' }
+  ];
+
+  //Labels shown on the x-axis
+  lineChartLabels: Label[] = ['23-04-2021', '22-04-2021', '21-04-2021', '20-04-2021', '19-04-2021', '18-04-2021'];
+
+  // Define chart options
+  lineChartOptions: ChartOptions = {
+    responsive: true
+  };
+
+  // Define colors of chart segments
+  lineChartColors: Color[] = [
+    { // dark grey
+      backgroundColor: 'white',
+      borderColor: '#2B5F8F',
+    },
+    { // red
+      backgroundColor: 'white',
+      borderColor: '#BC1D19',
+    }
+  ];
+
+  // Set true to show legends
+  lineChartLegend = true;
+
+  // Define type of chart
+
+  lineChartPlugins = [];
+
+  // events
+  lineChartClicked(event: any) {
+    console.log(event);
+  }
+
+  lineChartHovered(event: any) {
+    console.log(event);
   }
 
 }
