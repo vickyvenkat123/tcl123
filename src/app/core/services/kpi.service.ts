7a0bbc4424f81207f53fe2bde8f3a9813c729549
vickyvenkat123 <venkatvenky0151@gmail.com>
2021-06-08 20:00:50 -0700

    added my project

--- src/app/core/services/kpi.service.ts
+++ src/app/core/services/kpi.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,26 @@ import { Injectable } from '@angular/core';
 })
 export class KpiService {
 
-  constructor() { }
+ 
+ 
+
+  constructor(private http :HttpClient) { }
+
+  getkpiBell(){
+    return this.http.get('https://demo.emptracker.iot.tatacommunications.com/api/v1/userservice/customers')
+  }
+  getkpiSOS(){
+    return this.http.get('https://demo.emptracker.iot.tatacommunications.com/api/v1/userservice/customers/5d026850fbf97000016896cc')
+  }
+  getkpiBattery(){
+    return this.http.get('https://demo.emptracker.iot.tatacommunications.com/api/v1/getLoggedInUser')
+  }
+  // requestDataFromMultipleSources(): Observable<any[]> {
+  //   const response1 = this.getkpiBell(),
+  //     response2 = this.getkpiSOS(),
+  //     response3 = this.getkpiBattery();
+  //   // Observable.forkJoin (RxJS 5) changes to just forkJoin() in RxJS 6
+  //   return forkJoin([response1, response2, response3]);
+  // }
+
 }
